d148631f05211d7122009eb5b5b03e973c775144
Spencer Ridley <sridley4@uwo.ca>
2017-11-18 18:55:53 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/kumailn/budgetapp/MainActivity.java

--- app/src/main/java/com/kumailn/budgetapp/Main3Activity.java
+++ app/src/main/java/com/kumailn/budgetapp/Main3Activity.java
@@ -1,7 +1,12 @@
 package com.kumailn.budgetapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button button = (Button) findViewById(R.id.signupButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Signup();
+            }
+        });
+    }
+
+    public void Signup()
+    {
+        EditText category
     }
 }
