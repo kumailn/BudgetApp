a614fb02e7e54376016565a1b2e75bfcca781a87
Kumail Naqvi <kumailmn@gmail.com>
2017-11-18 06:01:37 -0500

    service update

--- app/src/main/java/com/kumailn/budgetapp/LocationService.java
+++ app/src/main/java/com/kumailn/budgetapp/LocationService.java
@@ -1,22 +1,16 @@
 package com.kumailn.budgetapp;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -25,51 +19,65 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-    String[] convertResult = new String[1];
-    public static String parseConvertResult;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        Intent i = new Intent(getApplicationContext(), LocationService.class);
-        startService(i);
+public class LocationService extends Service {
+    public LocationService() {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.settings){
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e("Service", "Started");
 
-            Toast.makeText(getApplicationContext(), "Click Works", Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                parseJSON();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        return START_STICKY;
     }
 
     com.android.volley.RequestQueue requestQueue;
     public String parseJSON(){
         requestQueue = Volley.newRequestQueue(this);
         //String jsonURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.97529585290097,-81.32619448150638&radius=500&type=store&keyword=store&key=AIzaSyCWR1MBKg3N4DDdrpsQ4hhQkcUPVBBNMCE";
-        String jsonURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.97529585290097,-82.32619448150638&radius=500&type=store&keyword=store&key=AIzaSyCWR1MBKg3N4DDdrpsQ4hhQkcUPVBBNMCE";
+        String jsonURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.97529585290097,-81.32619448150638&radius=500&type=store&keyword=store&key=AIzaSyCWR1MBKg3N4DDdrpsQ4hhQkcUPVBBNMCE";
+        final String[] aaa = {""};
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, jsonURL,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String aaa = response.getJSONArray("results").getJSONObject(0).getString("name");
+                            aaa[0] = response.getJSONArray("results").getJSONObject(0).getString("name");
                             //JSONArray jsonArray = response.getJSONArray("name");
                             //Toast.makeText(MainActivity.this, "JSON WORKS", Toast.LENGTH_SHORT).show();
-                            Log.e("JSONVOLLEY", aaa);
+                            Log.e("JSONVOLLEY", aaa[0]);
                         } catch (JSONException e) {
                             e.printStackTrace();
                             Log.e("No results found", "");
@@ -85,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         requestQueue.add(jsonObjectRequest);
-        return parseConvertResult;
+        return aaa[0];
     }
-
 }
