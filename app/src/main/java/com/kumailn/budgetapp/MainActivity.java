0949a1313d44ffcd5ff45e5dbc454e6dc4799da6
Kumail Naqvi <kumailmn@gmail.com>
2017-11-18 01:29:06 -0500

    Initial commit

---
+++ app/src/main/java/com/kumailn/budgetapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.kumailn.budgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.Settings){
+
+            Toast.makeText(getApplicationContext(), "Click Works", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
