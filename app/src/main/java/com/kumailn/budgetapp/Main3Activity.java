6c3536d89623ce0a6fb5bd8d9cb75b15c469f599
Aamir Tahir <saamirt@gmail.com>
2017-11-18 23:42:01 -0500

    Improved layout of initial setup activity

--- app/src/main/java/com/kumailn/budgetapp/Main3Activity.java
+++ app/src/main/java/com/kumailn/budgetapp/Main3Activity.java
@@ -14,7 +14,7 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
+        //csajsk
     }
 
     public void Signup()
